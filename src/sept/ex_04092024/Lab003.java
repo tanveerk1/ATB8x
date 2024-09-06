01bfec9974506f5bf59347431214253931f6e6da
USER <tanveerahmad.n12@gmail.com>
2024-09-06 07:38:11 +0530

    commit

---
+++ src/sept/ex_04092024/Lab003.java
@@ -0,0 +1,4 @@
+package sept.ex_04092024;
+
+public class Lab003 {
+}
---
+++ src/sept/ex_06092024/Lab004.java
@@ -0,0 +1,9 @@
+package sept.ex_06092024;
+
+public class Lab004 {
+}
+
+public static void main(String[] args) {
+system.out.printIn ("Hello Tanveer");
+}
+
